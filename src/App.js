3b2108d22aa1dbc8f1cca31530246723396fa0ed
Buggy-ARNOB <81729278+Buggy-ARNOB@users.noreply.github.com>
2022-12-12 17:18:45 +0600

    Environment DOne

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  console.log("j")
+  return (
+    <div>
+      console.console.log();
+    </div>
+  );
+}
+
+export default App;
