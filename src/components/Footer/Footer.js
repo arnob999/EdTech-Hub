5578cb1066d51197986f3ee89a4a179576ca055a
Buggy-ARNOB <81729278+Buggy-ARNOB@users.noreply.github.com>
2022-12-13 15:04:53 +0600

    Footer Added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='App'>
-            <p>This is Footer</p>
+        <div className='App bg-dark'>
+            <small className='text-white'>
+
+                copyright © 2022 || all rights reserved
+            </small>
+            <br />
+            <a className='text-decoration-none text-white' href="https://github.com/Buggy-ARNOB">ARNOB</a>
         </div>
     );
 };
