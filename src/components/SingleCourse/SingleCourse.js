94e6e00d4173edd12d25e1909203e4485b23c751
Buggy-ARNOB <81729278+Buggy-ARNOB@users.noreply.github.com>
2022-12-17 12:08:37 +0600

    Responsive

--- src/components/SingleCourse/SingleCourse.js
+++ src/components/SingleCourse/SingleCourse.js
@@ -6,14 +6,14 @@ const SingleCourse = ({ course }) => {
     const { course_id, title, pic, intro, details } = course;
 
     return (
-        <div className='my-5 d-flex courseCard mx-4 rounded-4 text-white'>
-            <>
+        <div className='my-5 w-100 row courseCard rounded-4 text-white'>
+            <div className='col-12 col-lg-2'>
                 <img className='courseImg' src={pic} alt="" />
-            </>
-            <div className='px-5 py-3'>
+            </div>
+            <div className='px-5 py-3 col-12 col-lg-10'>
                 <h4 className='pb-3'>{title}</h4>
                 <h5>{intro}</h5>
-                <p className='text-justify'>{details.slice(0, 250)}...Read More</p>
+                <p className='mx-auto w-50'>{details.slice(0, 100)}...Read More</p>
                 <Link to={`/courses/${course_id}`}>
                     <Button variant="primary">Go To Course</Button>
                 </Link>
